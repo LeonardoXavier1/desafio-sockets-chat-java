e0d0b01e4e75e2acb37dea6ef54ed3eda33c7419
viniubi <107428573+viniubi@users.noreply.github.com>
2023-07-30 18:47:51 -0300

    implement chat

--- app/src/main/java/com/example/chatsocket/MainActivity.java
+++ app/src/main/java/com/example/chatsocket/MainActivity.java
@@ -1,27 +1,18 @@
 package com.example.chatsocket;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Build;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.text.format.Formatter;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.chatsocket.serverstuff.Client;
-import com.example.chatsocket.serverstuff.Server;
-
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editIp;
-    EditText editPort;
-    Button btnConnect;
-    Button btnLaunch;
+    EditText editIp, editPort;
+    Button btnEnterServer, btnCreateServer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,33 +21,37 @@ public class MainActivity extends AppCompatActivity {
 
         editIp = findViewById(R.id.editIp);
         editPort = findViewById(R.id.editPort);
-        btnConnect = findViewById(R.id.btnConnect);
-        btnLaunch = findViewById(R.id.btnLaunch);
+        btnEnterServer = findViewById(R.id.btnEnterServer);
+        btnCreateServer = findViewById(R.id.btnCreateServer);
 
-        btnLaunch.setOnClickListener(view -> {
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        editIp.setText(Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress()));
+
+        btnCreateServer.setOnClickListener(view -> {
             int port = Integer.parseInt(editPort.getText().toString());
-
-            try {
-                ServerSocket serverSocket = new ServerSocket(port);
-                new Server(serverSocket).startServer();
-            } catch (IOException exception) {
-                Log.e("Server Launch", exception.getLocalizedMessage());
-            }
-        });
-
-        btnConnect.setOnClickListener(view -> {
-            int port = Integer.parseInt(editPort.getText().toString());
-            String ip = editIp.getText().toString();
-
-            Client client = new Client(ip, port, Build.MODEL);
-
-            Intent chatIntent = new Intent(
-                    MainActivity.this, ChatActivity.class
+            Server server = new Server(port);
+            connectToServer(
+                    editIp.getText().toString(),
+                    port
             );
-
-            chatIntent.putExtra("client", client);
-
-            startActivity(chatIntent);
         });
+
+        btnEnterServer.setOnClickListener(view -> {
+            connectToServer(
+                editIp.getText().toString(),
+                Integer.parseInt(editPort.getText().toString())
+            );
+        });
+    }
+
+    protected void connectToServer(String ip, int port) {
+        Intent chatIntent = new Intent(
+                MainActivity.this, ChatActivity.class
+        );
+
+        chatIntent.putExtra("ip", ip);
+        chatIntent.putExtra("port", port);
+
+        startActivity(chatIntent);
     }
 }
